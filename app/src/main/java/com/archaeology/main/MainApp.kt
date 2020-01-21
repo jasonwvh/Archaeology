f4b6c600ab39014909b3faf9b869c1cadd158371
haj37056 <jason.wongvh@gmail.com>
2020-01-21 12:18:06 +0100

    Revert "firebase feature"
    This reverts commit 4614709f03061dfda6360a5cda9b49f074d19bab.
    
    # Conflicts:
    #	app/src/main/java/com/archaeology/models/firebase/SiteFireStore.kt
    #	app/src/main/java/com/archaeology/views/BaseView.kt
    #	app/src/main/java/com/archaeology/views/login/LoginPresenter.kt
    #	app/src/main/java/com/archaeology/views/sitelist/SiteListPresenter.kt
    #	app/src/main/res/layout/activity_login.xml

--- app/src/main/java/com/archaeology/main/MainApp.kt
+++ app/src/main/java/com/archaeology/main/MainApp.kt
@@ -1,10 +1,10 @@
 package com.archaeology.main
 
 import android.app.Application
-import com.archaeology.models.SiteFireStore
+import com.archaeology.models.SiteStore
+import com.archaeology.models.room.SiteStoreRoom
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
-import com.archaeology.models.SiteStore
 
 class MainApp : Application(), AnkoLogger {
 
